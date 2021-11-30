277b725bcaeb3de5e677ca3b60d3b5e4e277c800
Walter González-Plata Cortes <waltergpc@hotmail.com>
2021-11-29 22:43:15 -0600

    intriduce security packages and not found middleware refactor for first deploy

--- app.js
+++ app.js
@@ -2,15 +2,34 @@ require('dotenv').config()
 require('express-async-errors')
 const express = require('express')
 const app = express()
+
+// Router
 const productsRouter = require('./routes/products')
+
+// Connect DB
 const connectDB = require('./db/connect')
+
+// Security packages
 const cors = require('cors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimiter = require('express-rate-limit')
+
+// Error middleware
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+)
 app.use(express.json())
-
+app.use(helmet())
 app.use(cors())
+app.use(xss())
 
 app.get('/', (req, res) => {
   res.send('store api')
@@ -23,7 +42,7 @@ app.use('/api/v1/bikes', productsRouter)
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 const start = async () => {
   try {
